510720493081e18dd0e7f5ab3c4a0bcfcca386a5
caprepo <vijayalakshmi.david@capgemini.com>
2017-08-18 19:48:35 +0530

    First Commit

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('production', {path: "/:start/to/:end"}, function() {
+    this.route('dimension', {path: "/dimension/:dimension_id"});
+  });
+
 });
 
 export default Router;
